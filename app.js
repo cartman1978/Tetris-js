4932b328527e322c46f683df50c2e9541c6cb326
cartman1978 <cartman_7802@yahoo.ie>
2021-02-25 20:46:48 +0000

    added javascript queryselector to the divs to start game functionality

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const ScroreDisplay = document.querySelector('#score');
+    const StartButton = document.querySelector('#start-button');
+    const width = 10;
+
+    console.log(squares);
+    
 
 });
